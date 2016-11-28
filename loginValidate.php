61caeb3c2a25b373e647798a3748af7228dc319d
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 15:47:03 -0800

    change

--- loginValidate.php
+++ loginValidate.php
@@ -2,12 +2,10 @@
 
 <?php
 $_SESSION["message"] = "";
-    if(isset($_POST["submit"])) {
         if ($_POST["password"] != "aaa" || $_POST["email"] != "a@a.a"){
             $_SESSION["message"] = "Invalid login";
             header("Location: login.php");
             die();
-        }
     }else {
         $_SESSION["valid"] = true;
         header("Location: index.php");
