f6a3debf5ad8305e9c6497af8861615811d6fdff
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 15:54:13 -0800

    change

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
         <title></title>
     </head>
     <body>
+        <h1>Please Login To Play</h1>
         <form method="post" action="loginValidate.php">
             <input type="text" id="email" name="email" placeholder="Email" />
             <input type="password" id="password" name="password" placeholder="Password" />
