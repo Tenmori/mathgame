e9d3d7398c5ada34e96290793e5f5250873f629e
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 09:02:31 -0800

    change

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
+<?php
+session_start();
+?>
+
 <html>
-
     <head>
-        <title></title>
+        <title>A00992565 Mathgame</title>
     </head>
-
     <body>
         <?php
-        $operarors = array( '+', '-', '*' );
-        $operator = $operarors[rand(0,2)];
-        $leftop = rand(0,20);
+        $operarors = array( '+', '-');
+        $operator = $operarors[rand(0,1)];
+        $leftop = $rand(0,20);
         $rightop = $rand(0,20);
 
         echo "the math: $leftop $operator $rightop = ??";
         ?>
-
     </body>
 </html>
