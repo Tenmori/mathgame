03cc2d94b54e3f68708ddd3f393cd0c37123686f
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 08:51:59 -0800

    change

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<?php
-$operarors = array( '+', '-', '*' );
-$operator = $operarors[rand(0,2)];
-$leftop = rand(0,20);
-$rightop = $rand(0,20);
+<html>
 
-echo "the math: $leftop $operator $rightop = ??";
-?>
+    <head>
+        <<title></title>
+    </head>
+
+    <body>
+        <?php
+        $operarors = array( '+', '-', '*' );
+        $operator = $operarors[rand(0,2)];
+        $leftop = rand(0,20);
+        $rightop = $rand(0,20);
+
+        echo "the math: $leftop $operator $rightop = ??";
+        ?>
+
+    </body>
+</html>
