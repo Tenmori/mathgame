81ee32b8b4a0ae3976790eee15534c0a088f7c17
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 08:36:14 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$operarors = array( '+', '-', '*' );
+$operator = $operarors[rand(0,2)];
+$leftop = rand(0,20);
+$rightop = $rand(0,20);
+
+echo "the math: $leftop $operator $rightop = ??";
+?>
