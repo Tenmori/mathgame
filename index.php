600667f766384dcf179250abc68973d23e0ae2da
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 12:13:02 -0800

    change

--- index.php
+++ index.php
@@ -8,19 +8,21 @@ session_start();
     </head>
     <body>
         <?php
-        $operarors = array( '+', '-');
-        $operator = $operarors[rand(0,1)];
+        $operator = rand(0,1);
+        $sign;
         $left = rand(0,20);
         $right = rand(0,20);
         $total =  $_SESSION["total"];
         $correct =  $_SESSION["correct"];
         if ($operator == 0){
             $answer = $left + $right;
+            $sign = "+";
         }else {
             $answer = $left - $right;
+            $sign = "-";
         }
 
-        echo "the math: $left $operator $right";
+        echo "the math: $left $sign $right";
         echo '<form method="post" action="validate.php">';
         echo '<input type="text" name="input" />';
         echo '<input type="submit" value="Check Answer" />';
