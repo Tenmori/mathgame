3bf3274d0986a54cfb6cd1043f595b0cca227d17
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 12:04:58 -0800

    changes

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ session_start();
         $operator = $operarors[rand(0,1)];
         $left = rand(0,20);
         $right = rand(0,20);
+        $total =  $_SESSION["total"];
+        $correct =  $_SESSION["correct"];
         if ($operator == 0){
             $answer = $left + $right;
         }else {
@@ -22,8 +24,9 @@ session_start();
         echo '<form method="post" action="validate.php">';
         echo '<input type="text" name="input" />';
         echo '<input type="submit" value="Check Answer" />';
-        echo '</form>'
-        
+        echo '</form>';
+        echo '$correct / $total';
+
         ?>
     </body>
 </html>
