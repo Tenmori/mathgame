f8c3e196a6b7b06cbfdcc1e247e14b176899f797
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 16:30:45 -0800

    change

--- index.php
+++ index.php
@@ -10,36 +10,38 @@ session_start();
     <body>
         <div class="container">
             <div class="col-sm-9 col-offset-3">
-                <?php
-                if (!isset($_SESSION["valid"]) || $_SESSION["valid"] != "true"){
-                    header("Location: login.php");
-                    die();
-                }
-                $operator = rand(0,1);
-                $sign;
-                $left = rand(0,20);
-                $right = rand(0,20);
-                $total =  $_SESSION["total"];
-                $correct =  $_SESSION["correct"];
-                if ($operator == 0){
-                    $_SESSION["answer"] = $left + $right;
-                    $sign = "+";
-                }else {
-                    $_SESSION["answer"] = $left - $right;
-                    $sign = "-";
-                }
-                echo "<h1>Math Game</h1>";
-                echo "$left $sign $right";
-                echo '<form method="post" action="validate.php">';
-                echo '<input type="text" name="input" />';
-                echo '<br/><br/><input type="submit" value="Check Answer" class="btn btn-primary"/>';
-                echo '</form>';
-                echo "0$correct / 0$total";
-                echo '<br /><br /><br />';
-                echo '<form method="post" action="logout.php">';
-                echo '<input type="submit" name="logout" value="Logout" class="btn btn-primary"/>';
+                <div class="col-sm-9 col-offset-3">
+                    <?php
+                    if (!isset($_SESSION["valid"]) || $_SESSION["valid"] != "true"){
+                        header("Location: login.php");
+                        die();
+                    }
+                    $operator = rand(0,1);
+                    $sign;
+                    $left = rand(0,20);
+                    $right = rand(0,20);
+                    $total =  $_SESSION["total"];
+                    $correct =  $_SESSION["correct"];
+                    if ($operator == 0){
+                        $_SESSION["answer"] = $left + $right;
+                        $sign = "+";
+                    }else {
+                        $_SESSION["answer"] = $left - $right;
+                        $sign = "-";
+                    }
+                    echo "<h1>Math Game</h1>";
+                    echo "$left $sign $right";
+                    echo '<form method="post" action="validate.php">';
+                    echo '<input type="text" name="input" />';
+                    echo '<br/><br/><input type="submit" value="Check Answer" class="btn btn-primary"/>';
+                    echo '</form>';
+                    echo "0$correct / 0$total";
+                    echo '<br /><br /><br />';
+                    echo '<form method="post" action="logout.php">';
+                    echo '<input type="submit" name="logout" value="Logout" class="btn btn-primary"/>';
 
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
     </body>
