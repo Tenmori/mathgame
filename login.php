dfb07941839001252f811d73fd3923ffefe46c05
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 16:10:42 -0800

    change

--- login.php
+++ login.php
@@ -7,12 +7,12 @@
     </head>
     <body>
         <div class="container">
-            <h1 class="col-sm-10">Please Login To Play</h1>
-            <div class="col-sm-10">
+            <h1 class="col-sm-12">Please Login To Play</h1>
+            <div class="col-sm-12">
                 <form method="post" action="loginValidate.php">
                     <input type="text" id="email" name="email" placeholder="Email" />
                     <input type="password" id="password" name="password" placeholder="Password" />
-                    <input type="submit" name ="submit" value="login" />
+                    <input type="submit" name ="submit" value="login" class="btn btn-primary"/>
                 </form>
             </div>
             <?php
