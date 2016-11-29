bc1d13021ceeb06bf138e37ead686ba560c3b41d
Tian Kirkpatrick <Tian Kirkpatrick>
2016-11-28 16:48:55 -0800

    change

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ session_start();
     </head>
     <body>
         <div class="container">
-            <div class="col-sm-9 col-sm-offset-3">
-                <div class="col-sm-11 col-sm-offset-1">
+            <div class="col-sm-9 col-offset-3">
+                <div class="col-sm-9 col-offset-3">
                     <?php
                     if (!isset($_SESSION["valid"]) || $_SESSION["valid"] != "true"){
                         header("Location: login.php");
@@ -30,19 +30,16 @@ session_start();
                         $sign = "-";
                     }
                     echo "<h1>Math Game</h1>";
-                    echo '<div class="col-sm-2 col-sm-offset-10">';
-                    echo '<form method="post" action="logout.php">';
-                    echo '<input type="submit" name="logout" value="Logout" class="btn btn-primary"/>';
-                    echo '</div>';
-                    echo '<br/>';
-                    echo "<br />$left $sign $right";
+                    echo "$left $sign $right";
                     echo '<form method="post" action="validate.php">';
-                    echo '<br/><input type="text" name="input" />';
+                    echo '<input type="text" name="input" />';
                     echo '<br/><br/><input type="submit" value="Check Answer" class="btn btn-primary"/>';
                     echo '</form>';
                     echo '<hr />';
                     echo "0$correct / 0$total";
                     echo '<br /><br /><br />';
+                    echo '<form method="post" action="logout.php">';
+                    echo '<input type="submit" name="logout" value="Logout" class="btn btn-primary"/>';
 
                     ?>
                 </div>
